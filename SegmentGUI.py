1ee99278a7819b9dadb7c2b768a1cba9fa1de960
RichardJWebb <rwebb444@gmail.com>
2020-11-25 09:13:55 -0800

    Changed name of txt file

--- SegmentGUI.py
+++ SegmentGUI.py
@@ -2,7 +2,7 @@ from tkinter import filedialog, Tk, Label, Button, StringVar, OptionMenu, Entry,
 import ENSC424projectClass
 import ntpath
 import os
-#import shutil
+import shutil
 
 OPTIONS = [
 "Bear",
@@ -87,12 +87,12 @@ class Application:
         print(file)
         Video.readVideo(file)
         (filename, ext) = os.path.splitext(ntpath.basename(file))
-        temp_filename = filename + ".txt"
+        temp_filename = filename + "_segmented.txt"
         filename = filename+"_segmented.avi"
         filepath = self.ent2.get() + "/"+filename
         filepath.encode('unicode_escape')
         Video.image_runthrough(self.variable.get(), vidName=filepath)
-        Video.output_interest_timestamp(temp_filename)
+        Video.output_interest_timestamp(temp_filename, self.ent2.get()+"/")
         Video.proper_end()
         self.master.quit()
 
