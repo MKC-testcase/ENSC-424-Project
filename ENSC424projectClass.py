603ca428f4485a2cde506bb2d9b168098d22010f
Marcus Chan <mkc28@sfu.ca>
2020-11-24 18:12:00 -0800

    txt_files
    added time segments of object of interest to video folder doesn't quite work like expected however it seems to work

--- ENSC424projectClass.py
+++ ENSC424projectClass.py
@@ -2,10 +2,15 @@
 The purpose of this file is to formally transfer what was tested in Yolo test and formally input it into a
 easily accessible class
 By: Marcus Chan
+Used: https://(medi)um.com/@iKhushPatel/convert-video-to-images-images-to-video-using-opencv-python-db27a128a481
+https://www.youtube.com/watch?v=1LCb1PVqzeY&ab_channel=eMasterClassAcademy
+https://www.youtube.com/watch?v=DLngCtsG3bk&feature=emb_logo&ab_channel=eMasterClassAcademy
 """
 import cv2
 import numpy as np
 import os
+import math
+import shutil
 #from os.path import isfile, join
 
 class media_interpret:
@@ -20,6 +25,8 @@ class media_interpret:
         self.confidences = []
         #variables above help the function of YOLO object determination to run
         self.frame_array = []
+        self.in_frame = [] # numbers in here should come in pairs of 2
+        self.fobj = False
 
     def setupFold(self):
         current_dir = os.getcwd()
@@ -111,7 +118,6 @@ class media_interpret:
 
             output_layers_names = self.net.getUnconnectedOutLayersNames()
             self.layerOutputs = self.net.forward(output_layers_names)
-            print(self.layerOutputs)
             self.classify_boxes()
             self.draw_boxes()
 
@@ -120,11 +126,22 @@ class media_interpret:
                 if object == self.classes[clas]:
                     if self.vidSetup == True:
                         self.vid_convert(self.vidSetup)
+                        #keeping track of the time frames that object is in frame
+                        if self.fobj == False:
+                            self.in_frame.append(x)
+                            self.fObj = True
                         break
                     else:
                         self.acquired_frame(count, 'test2')
                         break
+                if clas != self.class_ids[-1] and self.fobj == True:
+                    self.fobj == False
+                    self.in_frame.append(x)
             count = count + 1
+
+            if (x == self.frame_count):
+                self.fobj == False
+                self.in_frame.append(x)
 
             cv2.imshow('Image', self.image_frame)
             key = cv2.waitKey(1)
@@ -166,7 +183,28 @@ class media_interpret:
         else:
             print("You have not enabled the video segmentation option")
 
-     #https://(medi)um.com/@iKhushPatel/convert-video-to-images-images-to-video-using-opencv-python-db27a128a481
+    def output_interest_timestamp(self, txt_file):
+        count = 0
+        new_file = open(txt_file, "w+")
+        for read in self.in_frame:
+            beginning = read
+            end = beginning
+            count = count + 1
+            if (count%2) == 0:
+                fps1 = math.floor(beginning/self.fps)
+                fps2 = math.ceil(end/self.fps)
+                sec1 = math.floor(fps1%60)
+                min1 = math.floor((fps1/60)%60)
+                hour1 = math.floor((fps1/(60*60)))
+                sec2 = math.floor(fps2 % 60)
+                min2 = math.floor((fps2 / 60) % 60)
+                hour2 = math.floor((fps2 / (60 * 60)))
+                new_file.write("object of interest in {}:{}:{}--{}:{}:{} \n".format(hour1,min1,sec1,hour2,min2,sec2))
+        new_file.close()
+        current_dir = os.getcwd()
+        path = "{}/videos/{}".format(current_dir, txt_file)
+        current_dir = "{}/{}".format(current_dir,txt_file)
+        shutil.move(current_dir, path)
 
     def proper_end(self):
         self.cap.release()
@@ -178,7 +216,8 @@ if __name__ == '__main__':
     test = media_interpret()
     test.setupFold()
     test.readVideo('road.mp4')
-    test.image_runthrough('deer', vidName='test2_vid.avi')
+    test.image_runthrough('deer', vidName='test3_vid.avi')
+    test.output_interest_timestamp('test3_vid.txt')
     test.proper_end()
 
     #for the future purhaps explore the possibility to compress the jpg files automatically --> store as txt file
